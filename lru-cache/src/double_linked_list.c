5b438bf2bc4d50258adfb545f079f0f3477bf860
Jacob <jacob@notactivelylooking.com>
2024-04-16 13:11:53 +0100

    basic method for creating a new node.

--- lru-cache/src/double_linked_list.c
+++ lru-cache/src/double_linked_list.c
@@ -1,1 +1,11 @@
+#include <stdlib.h>
+
 #include "double_linked_list.h"
+
+node * newNode(int id) {
+    node *n = (node *)malloc(sizeof(node));
+    n->id = id;
+    n->parent = NULL;
+    n->child = NULL;
+    return n;
+}
