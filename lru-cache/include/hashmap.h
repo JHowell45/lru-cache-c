10366a52efa523235fedc631869a1a7081a4e703
Jacob <jacob@notactivelylooking.com>
2024-04-18 01:02:02 +0100

    initial basic setup.

--- lru-cache/include/hashmap.h
+++ lru-cache/include/hashmap.h
@@ -1,8 +1,23 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-typedef struct hashmap {
+#include <stdlib.h>
 
+typedef struct bucket {
+    char* key;
+    int value;
+} bucket;
+
+typedef struct hashmap {
+    size_t capacity;
+    size_t count;
+    bucket **buckets;
 } hashmap;
+
+int hashmapSearch(hashmap *map, char* key);
+
+void hashmapInsert(hashmap *map, char* key, int value);
+
+void hashmapDelete(hashmap *map, char* key);
 
 #endif
