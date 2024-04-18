be1a84f07d3bb4794ec3c4e9664aaeadd67bb9d7
Jacob <jacob@notactivelylooking.com>
2024-04-18 01:03:53 +0100

    internal private method for creating buckets.

--- lru-cache/src/hashmap.c
+++ lru-cache/src/hashmap.c
@@ -2,3 +2,10 @@
 #include <string.h>
 
 #include "hashmap.h"
+
+static bucket * newBucket(const char *key, int value) {
+    bucket *item = (bucket *)malloc(sizeof(bucket));
+    item->key = strdup(key);
+    item->value = value;
+    return item;
+}
